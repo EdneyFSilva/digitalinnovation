5bb537c807bbf0ef219f991eac0f3c1500bab175
EdneyFarias <edney.sic@hotmail.com>
2021-02-26 21:03:35 -0400

    definições de membros da classe

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -3,17 +3,28 @@ package one.digitalinnovation.digitalonebank
 class Pessoa {
     var nome:String = "Edney"
     var cpf:String = "000.000.000-00"
+        // tornando o set do atributo private, o mesmo pode ser feito para o get,
+        // dessa forma ajuda no controle de get/set
+        private set
 
-    inner class endereco{
+    inner class Endereco{
         var rua:String = "Rua 8"
         var num:Int = 33
     }
+
+    constructor()
+
+    fun uniNomeCpf() = "$nome e $cpf"
+
 }
 
 fun main(){
-    var edney = Pessoa()
-    edney.cpf = "406.285.232-15"
+    val edney = Pessoa()
+    edney.nome = "Joca"
+    //edney.cpf = "406.285.232-15"
     println(edney.nome)
     println(edney.cpf)
-    println(edney.endereco().rua)
+    println(edney.Endereco().rua)
+    println(edney.Endereco().num)
+    println(edney.uniNomeCpf())
 }
