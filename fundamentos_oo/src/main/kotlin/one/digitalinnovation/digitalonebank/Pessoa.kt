2f45d9a419e99818ee70f4244c2437b96886baf7
EdneyFSilva <edney.sic@hotmail.com>
2021-03-03 10:56:38 -0400

    transformação da classe Pessoa em abstract

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digitalonebank/Pessoa.kt
@@ -1,30 +1,7 @@
 package one.digitalinnovation.digitalonebank
 
-class Pessoa {
-    var nome:String = "Edney"
-    var cpf:String = "000.000.000-00"
-        // tornando o set do atributo private, o mesmo pode ser feito para o get,
-        // dessa forma ajuda no controle de get/set
-        private set
-
-    inner class Endereco{
-        var rua:String = "Rua 8"
-        var num:Int = 33
-    }
-
-    constructor()
-
-    fun uniNomeCpf() = "$nome e $cpf"
-
-}
-
-fun main(){
-    val edney = Pessoa()
-    edney.nome = "Joca"
-    //edney.cpf = "406.285.232-15"
-    println(edney.nome)
-    println(edney.cpf)
-    println(edney.Endereco().rua)
-    println(edney.Endereco().num)
-    println(edney.uniNomeCpf())
-}
+// Classe abstract não aceita instanciação
+abstract class Pessoa(
+    val nome:String,
+    val cpf:String
+)
